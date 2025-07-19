a1cd38e3342c009a74aa4feff4b39697769ca303
DiamoQ <diamond220qwert@mail.ru>
2025-07-19 09:56:40 +0500

    first commit

---
+++ src/sections/OrderSteps/stepsitems.js
@@ -0,0 +1,27 @@
+import deviceImgSrc_1 from '@/assets/images/devices/smartphones.svg'
+import deviceImgSrc_2 from '@/assets/images/devices/tablet.svg'
+import deviceImgSrc_3 from '@/assets/images/devices/smarttv.svg'
+import deviceImgSrc_4 from '@/assets/images/devices/laptops.svg'
+import deviceImgSrc_5 from '@/assets/images/devices/consoles.svg'
+import deviceImgSrc_6 from '@/assets/images/devices/vr.svg'
+
+const devicesItems = [
+    {
+        title: 'Выберите продукты и добавьте их в корзину',
+        imgSrc: deviceImgSrc_1,
+    },
+    {
+        title: 'Выберите способ приготовления',
+        imgSrc: deviceImgSrc_2,
+    },
+    {
+        title: 'Оформите заказ онлайн  или по телефону',
+        imgSrc: deviceImgSrc_3,
+    },
+    {
+        title: 'Ожидайте доставку  в ближайшее время',
+        imgSrc: deviceImgSrc_4,
+    },
+];
+
+export default devicesItems;
